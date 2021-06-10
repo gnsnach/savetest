90db65d413794fd837176ae112ca506c63e7e38a
Ganesh <gnsnach@gmail.com>
2021-06-10 18:51:15 +0800

    commit on master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { SearchComponent } from './search/search.component';
+import { GifsComponent } from './gifs/gifs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    SearchComponent,
+    GifsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,   
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getTrendingGifs() {
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=%$[environment.giphyApiKey]&limit=25`);
+ 
+ 
+ 
+  }
+}
