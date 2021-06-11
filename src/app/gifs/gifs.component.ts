8e09e303151ed09d85d8be1a5eaa4c084a54466d
Ganesh <gnsnach@gmail.com>
2021-06-11 19:44:53 +0800

    commitonmaster

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -7,18 +7,23 @@ import { DataService } from '../data.service';
   templateUrl: './gifs.component.html',
   styleUrls: ['./gifs.component.css']
 })
+
 export class GifsComponent implements OnInit {
 gifs: any[] = [];
   subscription!: Subscription;
+  message = "message from search";
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+
     this.dataService.getTrendingGifs();
     this.subscription = this.dataService.getGifs()
      .subscribe((response: any) => {
      //console.log('Data', response);
-      // console.log(response)
+      console.log(response)
+      //console.log(response.data) // will show the no of searches done so far in console, but need this to be updated in navbar
       this.gifs = response;
+      
     });
   
 }
